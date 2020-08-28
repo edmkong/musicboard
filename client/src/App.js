0cdcd59ce7ae21a50ca8a53e59352e1cc10e2bf1
Edmond Kong <edmondkong@Edmonds-MacBook-Pro.local>
2020-08-27 23:39:15 -0700

    Added live search, song queueing, and upvote functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 import queryString from 'query-string';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import VotingContainer from './Components/VotingContainer/VotingContainer'
 
-var fakeServerData = {
-  user:{
-    name: 'Edmond',
-    songs: [
-      {
-        name: 'Song 1!',
-        artist: 'Artist1',
-        duration: 360
-      },
-      {
-        name: 'Song 2',
-        artist: 'Artist2',
-        duration: 240
-      },
-      {
-        name: 'Song 3',
-        artist: 'Artist3',
-        duration: 200
-      }
-    ]
+var parsed = queryString.parse(window.location.search)
+var accessToken = parsed.access_token;
+var refreshToken = parsed.refresh_token;
+
+// var fakeServerData = {
+//   user:{
+//     name: 'Edmond',
+//     songs: [
+//       {
+//         name: 'Song 1!',
+//         artist: 'Artist1',
+//         duration: 360
+//       },
+//       {
+//         name: 'Song 2',
+//         artist: 'Artist2',
+//         duration: 240
+//       },
+//       {
+//         name: 'Song 3',
+//         artist: 'Artist3',
+//         duration: 200
+//       }
+//     ]
+//   }
+// };
+class SearchBar extends  React.Component {
+	constructor(props) {
+		super(props);
+		this.handleInputChange = this.handleInputChange.bind(this);
   }
+
+  handleInputChange(event) { 
+    this.props.onInputChange(event.target.value);
+  }
+
+   render() {
+		return (
+			<div className="container">
+				<h2 className="heading">Search for a song</h2>
+				<form>
+					<input
+						type="text"
+            placeholder="Search by song name..."
+            searchTerm = {this.props.searchTerm}
+						onChange = {this.handleInputChange}
+					/>
+					<button> GO </button>
+				</form>
+			</div>
+			);
+	  }
 }
 
 class SongCount extends Component{
@@ -46,16 +81,7 @@ class HourCounter extends Component{
     );
   }
 }
-class SearchBar extends Component{
-  render() {
-    return (
-      <div>
-        <img/>
-        <input type='text'/> Search
-      </div>
-    );
-  }
-}
+
 // playlist with all songs, display current song?
 class Playlist extends Component{
   render() {
@@ -63,11 +89,11 @@ class Playlist extends Component{
       <div>
         <img/>
         <h3>Playlist</h3>
-        <ul>
+        {/* <ul>
           {this.props.songs.map(song =>
             <li>{song.name + ": " + song.artist}</li>
           )}
-        </ul>
+        </ul> */}
 
       </div>
     );
@@ -76,38 +102,111 @@ class Playlist extends Component{
 class App extends Component{
   constructor() {
     super();
-    this.state = {serverData: {},
-    searchString: ''
+    this.state = {
+    serverData: {
+      user:{
+        name: '',
+        playlist: {}
+      }
+    },
+    searchTerm: '',
+    searchResults: [],
+    playlist: [],
+    votingPanel: []
     };
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleUpvote = this.handleUpvote.bind(this)
+  };
+
+  fetchSearchResults(query) {
+    const searchURL = 
+    'https://api.spotify.com/v1/search?q=' + query + '&type=track';
+    
+    fetch(searchURL, {
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    }).then(response => response.json())
+    // .then(data => console.log(data.tracks.items))
+    .then(data => this.setState({
+      searchResults: data.tracks.items.map( item => {
+        return {
+          name: item.name,
+          artist: item.artists[0].name,
+          id: item.id
+        }
+    })
+    }))
+    
+  }
+
+  handleInputChange(searchTerm) {
+    if (!searchTerm) {
+      this.setState({
+        searchTerm: ''
+      });
+    } else {
+      this.setState({
+        searchTerm:searchTerm
+      });
+      this.fetchSearchResults(searchTerm);
+    }
+  };
+
+  addSongToPanel(song) {
+    this.setState({
+      votingPanel: [...this.state.votingPanel, 
+      {name: song.name, artist: song.artist, id:song.id, votes: 0}]});
+  }
+
+  handleUpvote(track) {
+    track.votes ++
+    //set state of Voting Panel, prioritizing highest voted songs
+    var newList = this.state.votingPanel.slice()
+    newList.sort((a, b) => (a.votes > b.votes) ? -1 : 1)
+    this.setState({votingPanel: newList})
   }
 
   componentDidMount() {
-    var parsed = queryString.parse(window.location.search)
-    var accessToken = parsed.access_token;
-    var refreshToken = parsed.refresh_token;
     //returns a promise
-    fetch('https://api.spotify.com/v1/me/playlists', {
+    fetch('https://api.spotify.com/v1/me', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
     }).then(response => response.json())
-    .then(data => console.log(data))
-    //.then(data => this.setState({serverData: {user: {name: data.display_name}}}))
+    // .then(data => console.log(data))
+    .then(data => this.setState({serverData: {user: {name: data.display_name}}}))
   }
   render() {
     return (
       <div className="App">
         {this.state.serverData.user ?
         <div>
-          <h1>
-            {this.state.serverData.user.name}'s Musicboard
-          </h1>
-          <SongCount songs={this.state.serverData.user.songs}/>
-          <HourCounter songs={this.state.serverData.user.songs}/>
-          <SearchBar>
+          <Container>
+            <Row>
+              <h1>
+                {this.state.serverData.user.name}'s Musicboard
+              </h1>
+              {/* <SongCount songs={this.state.serverData.user.songs}/>
+              <HourCounter songs={this.state.serverData.user.songs}/> */}
+            </Row>
+            <Row>
+              <Col>
+                <VotingContainer
+                  tracks = {this.state.votingPanel}
+                  upvote = {this.handleUpvote}
+                />
+              </Col>
+              <Col>
+                <SearchBar
+                  searchTerm = {this.state.searchTerm}
+                  onInputChange = {this.handleInputChange}
+                  />
+                  {this.state.searchResults.map((song, index) => (
+                    <li key={song.id}>
+                    <button onClick={() => this.addSongToPanel(song)}>Add to Queue</button>Song:{song.name} Artist:{song.artist}
+                     </li>
+                  ))}
+              </Col>
+            </Row>
+          </Container>
 
-          </SearchBar>
-          <Playlist songs={this.state.serverData.user.songs}>
-
-          </Playlist>
         </div> : <button onClick ={() => window.location =
                 'http://localhost:8888/login'}>Sign in with Spotify
                 </button>
